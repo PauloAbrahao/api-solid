2c617bc2b8a3d39e2e49c9264d98f1e6abc07420
PauloAbrahao <ab.cod@outlook.com>
2024-11-25 19:55:20 -0300

    feat: implement repository pattern

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,28 @@
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repositories'
+import { RegisterUseCase } from '@/services/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
